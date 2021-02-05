6af8134c107939abd8ef7cae5905562207d3089e
Eike Falkenberg <efalkenberg@gmail.com>
2021-02-04 20:20:19 -0800

    Fun with stacks

---
+++ coding_exercises/linked_stacks_and_queues.py
@@ -0,0 +1,86 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+class Stack(object):
+
+    def __init__(self, data=None, next_node=None):
+        self.data = []
+
+    def push(self, data):
+        self.data.append(data)
+
+    def pop(self):
+        if len(self.data) > 0:
+            return_value = self.data[len(self.data)-1]
+            self.data.pop(len(self.data)-1)
+            return return_value
+        return None
+    
+    def length(self):
+        return len(self.data)
+
+    def __repr__(self):
+        return str(self.data)
+
+
+class MultiStack(object):
+
+    def __init__(self, stacks=3, data=None):
+        self.data = [None] * (stacks * 5)
+        self.indices = []
+        for i in range(0, stacks, 1):
+            self.indices.append(int(len(self.data)/stacks)*i)        
+
+    def push(self, stack, data):
+        # this implementation completely lacks boundary
+        # checks and growth
+        self.data[self.indices[stack-1]] = data
+        self.indices[stack-1] += 1
+
+    def pop(self, stack):
+        self.indices[stack-1] -= 1
+        data = self.data[self.indices[stack-1]]
+        self.data[self.indices[stack-1]] = None
+        return data
+    
+    def length(self, stack):
+        # laaaaaazy 🦥
+        return int(len(self.data)/3)
+
+    def __repr__(self):
+        return str(self.data)
+
+class MinStack(object):
+        
+    def __init__(self):
+        self.data = []
+        self.min = None
+
+    def push(self, value):
+        self.data.append((value, self.min))
+        if self.min is None or self.min > value:
+            self.min = value
+    
+    def pop(self):
+        value = self.data.pop(len(self.data)-1)
+        # restore stack if needed
+        if value[0] == self.min:
+            self.min = value[1]
+        return value[0]
+    
+    def get_min(self):
+        return self.min
+
+    def __repr__(self):
+        return str(self.data) + " min:" + str(self.min)
+
+
+
+
+class StackUtils:
+
+    @staticmethod
+    def reverse_stack(input):
+        return input
+
+                
---
+++ tests/test_stacks_and_queues.py
@@ -0,0 +1,67 @@
+#!/usr/bin/python
+# -*- coding: utf-8 -*-
+
+import unittest
+import sys
+import os
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+
+import coding_exercises.linked_stacks_and_queues as sq
+
+class TestStack(unittest.TestCase):
+
+    def test_stack(self):
+        stack = sq.Stack()
+        stack.push(100)
+        stack.push(200)
+        stack.push(300)
+        stack.push(400)
+        self.assertEqual(stack.length(), 4)
+        self.assertEqual(stack.pop(), 400)
+        self.assertEqual(stack.pop(), 300)
+        self.assertEqual(stack.pop(), 200)
+        self.assertEqual(stack.pop(), 100)
+        self.assertEqual(stack.pop(), None)
+
+    def test_triple_stack(self):
+        stack = sq.MultiStack(3)
+        stack.push(1, 100)
+        stack.push(1, 200)
+        stack.push(1, 300)
+        stack.push(1, 400)
+
+        stack.push(2, 7)
+        stack.push(2, 8)
+        stack.push(2, 9)
+
+        stack.push(3, 1000)
+        stack.push(3, 2000)
+        stack.push(3, 3000)
+
+        self.assertEqual(stack.pop(1), 400)
+        self.assertEqual(stack.pop(1), 300)
+        self.assertEqual(stack.pop(1), 200)
+        self.assertEqual(stack.pop(1), 100)
+        self.assertEqual(stack.pop(1), None)
+        self.assertEqual(stack.pop(2), 9)
+        self.assertEqual(stack.pop(2), 8)
+        self.assertEqual(stack.pop(3), 3000)
+
+    def test_min_stack(self):
+        stack = sq.MinStack()
+        stack.push(100)
+        stack.push(200)
+        stack.push(300)
+        stack.push(400)
+        self.assertEqual(stack.min, 100)
+        stack.push(1)
+        self.assertEqual(stack.min, 1)
+        self.assertEqual(stack.pop(), 1)
+        self.assertEqual(stack.get_min(), 100)
+        self.assertEqual(stack.pop(), 400)
+        self.assertEqual(stack.get_min(), 100)
+    
+
+
+if __name__ == '__main__':
+    unittest.main()
